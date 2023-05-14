43ab6cd9160802c9734bc6a3b2a406be2c0a6cbe
coryjanowski <coryjanowski@gmail.com>
2023-05-14 04:21:34 -0600

    first merge update

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a generic PHP test file to test random PHP scripting and development. It is a AMP stack.
+
+# Point 2
+
+# Point 3
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>PHP Practice</h1>
+<p>HTML here is wrote as normal in HTML!</p>
+<?php echo "<p>But this code is interpreted by PHP and turned into HTML</p>";?>
+<?php echo "<ul><li>You can use any HTML tags!</li><li>like this list.</li></ul>";?>
+<footer>
+  <p>Coryj</p>
+
+  <input type="button" value="Say Hi!" onclick="location='test.php'" />
+</footer>
